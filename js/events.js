65ee738126018f0f54d01f08cb724e1a574e600f
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2021-12-06 00:27:10 +0530

    events completed

--- js/events.js
+++ js/events.js
@@ -6,3 +6,15 @@ document.getElementById('Heading').addEventListener('click', function(e){
     vari = e.offsetX;
     console.log(vari);
 });
+
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function(e){
+   e.preventDefault();
+   console.log("I'm working",e);
+});
+
+document.getElementById('container').addEventListener('mouseover', function(e){
+    document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},${e.offsetX})`;
+});
+
+// mouseenter mouseleave mousemove
